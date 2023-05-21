1d7d38be10691db93214eb9a6e219d9d4d729c5c
EvPllll <eugeneplyusnin@yandex.ru>
2023-05-22 02:32:50 +0700

    add script, i need more practice with OOP...

---
+++ multiple inheritance.py
@@ -0,0 +1,30 @@
+class Goods:
+    def __init__(self, name, weight, price):
+        super().__init__()
+        self.name = name
+        self.weight = weight
+        self.price = price
+
+    def show_info(self):
+        print(f'{"-" * 10}\nName: {self.name}\nWeight: {self.weight}\nPrice: {self.price}\n{"-" * 10}')
+
+
+class MixingLog:
+    ID = 0
+    def __init__(self):
+        self.ID += 1
+        self.id = self.ID
+
+    def show_log(self):
+        print(f'{self.id}: saled')
+
+class Notebook(Goods, MixingLog):
+    pass
+
+
+notebook = Notebook('Dell Latitude 3410', 1.5, 40000)
+notebook.show_info()
+notebook.show_log()
+second_notebook = Notebook('some_Asus', 2.0, 45000)
+second_notebook.show_info()
+second_notebook.show_log()
